a000da58bd8231a90264259c0aec613e072c39bb
Gustavo <gustavino_tavo@hotmail.com>
2017-05-11 22:56:47 -0300

    Se agrega funcion para cargar el menu desde un archivo individual

---
+++ Menu.php
@@ -0,0 +1,51 @@
+<!--<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript">
+	</script>
+</head>
+<body>-->
+	<input type="hidden" name="IdMenu" id="IdMenu" value="<?php echo $_POST['IdMenu'];?>">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="index.html">
+			BAM Tejendos Ideas
+		</a>
+	</div>
+	<div id="navbar" class="collapse navbar-collapse">
+		<ul role="presentation" class="nav navbar-nav">
+			<li role="presentation" class="active">
+				<a>
+					Misión
+				</a>
+			</li>
+		<li role="presentation">
+			<a href="Vision.html">
+				Visión
+			</a>
+		</li>
+		<li role="presentation">
+			<a href="TProductos.html">
+				Tipo de Productos
+			</a>
+		</li>
+		<li role="presentation">
+			<a href="Galeria.html">
+				Galerias
+			</a>
+		</li>
+		<li role="presentation">
+			<a href="Contacto.html">
+				Contacto
+			</a>
+		</li>
+		</ul>
+	</div>
+<!--</body>
+</html>-->
